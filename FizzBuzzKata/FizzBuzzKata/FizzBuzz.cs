85273b423a6d3e0b94ec5a79659ba363f3373a18
CHILLI\Mahesh <Mahesh.Moodley@chillisoft.co.za>
2018-02-20 17:10:53 +0200

    Add Fourth test

---
+++ FizzBuzzKata/FizzBuzzKata/FizzBuzz.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzKata
+{
+    public class FizzBuzz
+    {
+        public string PrintFizzBuzz(int input)
+        {
+           if (input == 3)
+            {
+                return "Fizz";
+            }
+            return input.ToString();
+        }
+    }
+}
